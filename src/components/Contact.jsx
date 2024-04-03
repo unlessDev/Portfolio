ee5867d709865f17b693a80d8532945b9baa9c93
unlessDev <unlessdev@gmail.com>
2024-04-03 04:05:18 -0300

    FramerMotions

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,42 @@
+import { motion } from "framer-motion";
+
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-        <h1 className="my-10 text-center text-4xl">Contáctame</h1>
-        <div className="text-center tracking-tighter">
-            <p className="my-4">
-                Ubicacion
-            </p>
-            <p className="my-4">
-                Telefono
-            </p>
-            <a href="" className="border-b">
-                Mail
-            </a>
-        </div>
+        <motion.h1 
+            whileInView={{ opacity: 1, x: 0}}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1.6 }}
+            className="my-10 text-center text-4xl">
+                Pongamonos En Contacto
+        </motion.h1>
+
+                <div className="text-center tracking-tighter">
+                    <motion.p 
+                        whileInView={{ opacity: 1, x: 0}}
+                        initial={{ opacity: 0, x: 100 }}
+                        transition={{ duration: 1.6 }}
+                        className="my-4">
+                            Ubicación
+                    </motion.p>
+
+                    <motion.p 
+                        whileInView={{ opacity: 1, x: 0}}
+                        initial={{ opacity: 0, x: -100 }}
+                        transition={{ duration: 1.6 }}
+                        className="my-4">
+                            Teléfono
+                    </motion.p>
+
+                    <motion.a 
+                        whileInView={{ opacity: 1, x: 0}}
+                        initial={{ opacity: 0, x: 100 }}
+                        transition={{ duration: 1.6 }}
+                        href="" 
+                        className="border-b">
+                            Mail
+                    </motion.a>
+                </div>
     </div>
   )
 }
