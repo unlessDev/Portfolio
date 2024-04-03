eb2ed5503cacac2f5988ed308c01647f1c746ca1
unlessDev <unlessdev@gmail.com>
2024-04-03 08:42:51 -0300

    Prettier

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,43 +1,48 @@
-import aboutImg from "../assets/anime2.jpeg";
+import aboutImg from "../assets/animeOscu.png";
 import { FaChevronDown } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 const About = () => {
   return (
-    <div className="border-b border-neutral-900 font-semibold pb-4">
-        <h1 className="my-20 text-blue-600 text-center text-4xl">
-            Sobre Mí
-        </h1>
+    <div className="border-b border-neutral-600 font-semibold pb-4">
+      <h1 className="my-20 text-center text-4xl">Sobre Mí</h1>
 
-        <div className="flex flex-wrap">
-            <motion.div 
-                whileInView={{ opacity: 1, x: 0 }}
-                initial={{ opacity: 0, x: -100 }}
-                transition={{ duration: 0.5 }}
-                className="w-full lg:w-1/2 lg:p-8">
-                    <div className="flex items-center justify-center">
-                        <img className="rounded-2xl" src={aboutImg} alt="Sobre mí" />
-                    </div>
-            </motion.div>
+      <div className="flex flex-wrap">
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
+          <div className="flex items-center justify-center">
+            <img className="rounded-2xl" src={aboutImg} alt="Sobre mí" />
+          </div>
+        </motion.div>
 
-            <motion.div 
-                whileInView={{ opacity: 1, x: 0 }}
-                initial={{ opacity: 0, x: 100 }}
-                transition={{ duration: 0.5 }}
-                className="w-full lg:w-1/2">
-                    <div className="flex justify-center lg:justify-start">
-                        <p className="my-2 maxw-xl py-6">
-                        Soy un desarrollador full stack dedicado y versátil, con pasión por crear aplicaciones web eficientes y amigables para el usuario. 
-                        Con 2 años de experiencia profesional, he trabajado con diversas tecnologías, como React, Next.js, Node.js, MySQL, PostgreSQL y MongoDB. 
-                        Mi camino en el desarrollo web comenzó con una profunda curiosidad por cómo funcionan las cosas, y ha evolucionado en una carrera 
-                        donde continuamente me esfuerzo por aprender y adaptarme a nuevos desafíos. Me destaco en entornos colaborativos y disfruto resolver 
-                        problemas complejos para entregar soluciones de alta calidad. Fuera del código, me gusta mantenerme activo, explorar nuevas 
-                        tecnologías y contribuir a proyectos de código abierto.
-                        </p>
-                    </div>
-            </motion.div>
-        </div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2"
+        >
+          <div className="flex items-center justify-center lg:justify-start">
+            <p className="my-2 max-w-xl py-6">
+              Soy un desarrollador full stack dedicado y versátil, con pasión
+              por crear aplicaciones web eficientes y amigables para el usuario.
+              Con 2 años de experiencia profesional, he trabajado con diversas
+              tecnologías, como React, Next.js, Node.js, MySQL, PostgreSQL y
+              MongoDB. Mi camino en el desarrollo web comenzó con una profunda
+              curiosidad por cómo funcionan las cosas, y ha evolucionado en una
+              carrera donde continuamente me esfuerzo por aprender y adaptarme a
+              nuevos desafíos. Me destaco en entornos colaborativos y disfruto
+              resolver problemas complejos para entregar soluciones de alta
+              calidad. Fuera del código, me gusta mantenerme activo, explorar
+              nuevas tecnologías y contribuir a proyectos de código abierto.
+            </p>
+          </div>
+        </motion.div>
+      </div>
     </div>
-  )
+  );
 };
-export default About
+export default About;
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -8,92 +8,100 @@ import { FaBootstrap } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
-    initial: { y: -10 },
-    animate: {
-        y:[10, -10],
-        transition: {
-            duration: duration,
-            ease: "linear",
-            repeat: Infinity,
-            repeatType: "reverse",
-        },
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
     },
+  },
 });
 
 const Technologies = () => {
   return (
-    <div className="border-b border-neutral-800 pb-24">
-        <motion.h2 
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: 100 }}
-            transition={{ duration: 1.5 }}
-            className="my-20 text-center text-4xl">
-                Tecnologías
-        </motion.h2>
-        <motion.div 
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: -100 }}
-            transition={{ duration: 1.5 }}
-            className="flex flex-wrap items-center justify-center gap-4">
-                <motion.div 
-                    variants={ iconVariants(2) }
-                    initial="initial"
-                    animate="animate"
-                    className="rounded-2xl border-4 border-neutral-800 p-4">
-                        <RiReactjsLine className="text-7xl text-cyan-400" />
-                </motion.div>
-
-                <motion.div 
-                    variants={ iconVariants(3) }
-                    initial="initial"
-                    animate="animate"
-                    className="rounded-2xl border-4 border-neutral-800 p-4">
-                        <TbBrandNextjs className="text-7xl text-white-500" />
-                </motion.div>
-
-                <motion.div 
-                    variants={ iconVariants(4) }
-                    initial="initial"
-                    animate="animate"
-                    className="rounded-2xl border-4 border-neutral-800 p-4">
-                        <FaAngular className="text-7xl text-red-600" />
-                </motion.div>
-
-                <motion.div                 
-                    variants={ iconVariants(5) }
-                    initial="initial"
-                    animate="animate"
-                    className="rounded-2xl border-4 border-neutral-800 p-4">
-                        <FaBootstrap className="text-7xl text-purple-600" />
-                </motion.div>
-
-                <motion.div 
-                    variants={ iconVariants(4) }
-                    initial="initial"
-                    animate="animate"
-                    className="rounded-2xl border-4 border-neutral-800 p-4">
-                        <FaNodeJs className="text-7xl text-green-600" />
-                </motion.div>
-
-                <motion.div 
-                    variants={ iconVariants(3) }
-                    initial="initial"
-                    animate="animate"
-                    className="rounded-2xl border-4 border-neutral-800 p-4">
-                        <BiLogoPostgresql className="text-7xl text-sky-700" />
-                </motion.div>
-
-                <motion.div 
-                    variants={ iconVariants(2) }
-                    initial="initial"
-                    animate="animate"
-                    className="rounded-2xl border-4 border-neutral-800 p-4">
-                        <SiMongodb className="text-7xl text-green-600" />
-                </motion.div>
-
+    <div className="border-b border-neutral-600 pb-24">
+      <motion.h2
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Tecnologías
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
         </motion.div>
+
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandNextjs className="text-7xl text-white-500" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaAngular className="text-7xl text-red-600" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaBootstrap className="text-7xl text-purple-600" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-green-600" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <BiLogoPostgresql className="text-7xl text-sky-700" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-green-600" />
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
 
-export default Technologies
+export default Technologies;
